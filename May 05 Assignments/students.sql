1433c7a2f56088f8b1bb7c014894b2c2e8ae8d6c
Manjunath <plmanjunath313@gmail.com>
2025-05-05 11:30:52 +0530

    students table

---
+++ May 05 Assignments/students.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    Marks INT,
+    DOB DATE
+);
+INSERT INTO Students (StudentID, FirstName, Marks, DOB)
+VALUES
+(1, 'Anit', 92, '2003-05-10'),
+(2, 'Neha', 78, '2002-11-15'),
+(3, 'Rahul', 61, '2004-01-20'),
+(4, 'Sneha', 49, '2003-09-01');
