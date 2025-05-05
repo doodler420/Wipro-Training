d1b055d20c07860a889ebd24d035a6e249fd9ffc
Manjunath <plmanjunath313@gmail.com>
2025-05-05 10:26:29 +0530

    code update

--- May 05 Assignments/Assignment_2.sql
+++ May 05 Assignments/Assignment_2.sql
@@ -1,17 +1,11 @@
-CREATE PROCEDURE GetProductRevenue @ProductID INT
-AS
+CREATE PROCEDURE GetProductRevenue @ProductID INT AS
 BEGIN
     SELECT 
         p.ProductID,
         p.ProductName,
         SUM(o.Quantity * p.Price) AS TotalRevenue
-    FROM 
-        Orders o
-    INNER JOIN 
-        Products p ON o.ProductID = p.ProductID
-    WHERE 
-        o.ProductID = @ProductID
-    GROUP BY 
-        p.ProductID, p.ProductName;
+    FROM Orders o
+    INNER JOIN Products p ON o.ProductID = p.ProductID
+    WHERE o.ProductID = @ProductID;
 END;
 EXEC GetProductRevenue @ProductID=1;
