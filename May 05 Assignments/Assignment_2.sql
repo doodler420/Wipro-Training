4bbb0894d4df24b992d94603b6afe8fc2c4b76ae
Manjunath <plmanjunath313@gmail.com>
2025-05-05 10:05:10 +0530

    assignment 2

---
+++ May 05 Assignments/Assignment_2.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE dbo.GetProductRevenue
+    @ProductID INT
+AS
+BEGIN
+    SELECT 
+        p.ProductID,
+        p.ProductName,
+        SUM(o.Quantity * p.Price) AS TotalRevenue
+    FROM 
+        Orders o
+    INNER JOIN 
+        Products p ON o.ProductID = p.ProductID
+    WHERE 
+        o.ProductID = @ProductID
+    GROUP BY 
+        p.ProductID, p.ProductName;
+END;
