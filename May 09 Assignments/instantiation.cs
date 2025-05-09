bb0c74325e16be964c86605544b2d3dcd565db79
Manjunath <plmanjunath313@gmail.com>
2025-05-09 10:31:13 +0530

    comments for notes

--- May 09 Assignments/instantiation.cs
+++ May 09 Assignments/instantiation.cs
@@ -1,7 +1,7 @@
 using System;
-//using ConsoleApp2;
+//using ConsoleApp2; You can use this instead of writing under namespace ConsoleApp1{} => Method 1 of Instantiation
 
-namespace ConsoleApp2
+namespace ConsoleApp1 //Method 2 of instantiation
 {
     class Program
     {
@@ -9,7 +9,9 @@ namespace ConsoleApp2
         {
             Student student1 = new Student { RollNumber = 101, Name = "Neha", Marks = 90 };
             Student student2 = new Student { RollNumber = 102, Name = "Ravi", Marks = 75 };
-
+            //if neither method1 or method2 is used,
+            //  use this , given class filename is Student =>  ConsoleApp2.Student student1 = new ConsoleApp2.Student
+            // {declare deets here};
             student1.PrintDetails();
             student2.PrintDetails();
         }
