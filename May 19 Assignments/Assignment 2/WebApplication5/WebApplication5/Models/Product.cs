5f7dbd737d419cc4cd118e3a5bab76ac971c3983
Manjunath <plmanjunath313@gmail.com>
2025-05-19 16:05:05 +0530

    migration notes and assignment

---
+++ May 19 Assignments/Assignment 2/WebApplication5/WebApplication5/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace WebApplication5.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        //public Category Category { get; set; }
+    }
+    //public class Category
+    //{
+    //    public int CategoryId { get; set; }
+    //    public string CategoryName { get; set; }
+    //    public ICollection<Product> Products { get; set; }
+    //}
+
+}
