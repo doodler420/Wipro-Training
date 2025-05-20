6298fc9c981612f09a670939bd2e03b9feb553cf
Manjunath <plmanjunath313@gmail.com>
2025-05-20 12:15:41 +0530

    assignments

---
+++ May 20 Assignments/Assignment 2/WebApplication7/WebApplication7/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication7.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
