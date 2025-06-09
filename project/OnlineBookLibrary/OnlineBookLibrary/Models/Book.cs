bf51e533b6a6f33a137d4080c73a5fde6a2e9683
Manjunath <plmanjunath313@gmail.com>
2025-06-09 10:51:43 +0530

    project

---
+++ project/OnlineBookLibrary/OnlineBookLibrary/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace OnlineBookLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        [Display(Name = "Published Year")]
+
+        public int PublishedYear { get; set; }
+
+        [Display(Name = "Is Available")]
+
+        public bool IsAvailable { get; set; }
+    }
+}
